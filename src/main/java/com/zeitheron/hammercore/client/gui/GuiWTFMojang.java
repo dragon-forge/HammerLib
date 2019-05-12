fedc32f1ae50332673ea6ff23a9ab806950ce728
Zeitheron <zeitheron.dr@gmail.com>
2019-05-12 16:09:43 +0300

    Fix GuiWTFMojang!

--- src/main/java/com/zeitheron/hammercore/client/gui/GuiWTFMojang.java
+++ src/main/java/com/zeitheron/hammercore/client/gui/GuiWTFMojang.java
@@ -2,16 +2,23 @@ package com.zeitheron.hammercore.client.gui;
 
 import com.zeitheron.hammercore.client.gui.impl.container.SlotScaled;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiLabel;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.TextFormatting;
 
-public class GuiWTFMojang<T extends Container> extends GuiContainer
+public abstract class GuiWTFMojang<T extends Container> extends GuiContainer
 {
 	protected boolean fixMojang = true;
 	
@@ -33,9 +40,129 @@ public class GuiWTFMojang<T extends Container> extends GuiContainer
 			drawDefaultBackground();
 			GlStateManager.color(1F, 1F, 1F, 1F);
 		}
-		super.drawScreen(mouseX, mouseY, partialTicks);
+		{
+			
+			int i = this.guiLeft;
+			int j = this.guiTop;
+			this.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
+			GlStateManager.disableRescaleNormal();
+			RenderHelper.disableStandardItemLighting();
+			GlStateManager.disableLighting();
+			GlStateManager.disableDepth();
+			{
+				for(int k = 0; k < this.buttonList.size(); ++k)
+					((GuiButton) this.buttonList.get(k)).drawButton(this.mc, mouseX, mouseY, partialTicks);
+				for(int l = 0; l < this.labelList.size(); ++l)
+					((GuiLabel) this.labelList.get(l)).drawLabel(this.mc, mouseX, mouseY);
+			}
+			RenderHelper.enableGUIStandardItemLighting();
+			GlStateManager.pushMatrix();
+			GlStateManager.translate((float) i, (float) j, 0.0F);
+			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+			GlStateManager.enableRescaleNormal();
+			this.hoveredSlot = null;
+			int k = 240;
+			int l = 240;
+			OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
+			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+			
+			for(int i1 = 0; i1 < this.inventorySlots.inventorySlots.size(); ++i1)
+			{
+				Slot slot = this.inventorySlots.inventorySlots.get(i1);
+				
+				if(slot.isEnabled())
+				{
+					this.drawSlot(slot);
+				}
+				
+				if(this.isMouseOverSlot(slot, mouseX, mouseY) && slot.isEnabled())
+				{
+					this.hoveredSlot = slot;
+					GlStateManager.disableLighting();
+					GlStateManager.disableDepth();
+					int j1 = slot.xPos;
+					int k1 = slot.yPos;
+					GlStateManager.colorMask(true, true, true, false);
+					if(slot instanceof SlotScaled)
+						this.drawGradientRect(j1, k1, j1 + ((SlotScaled) slot).getWidth(), k1 + ((SlotScaled) slot).getHeight(), -2130706433, -2130706433);
+					else
+						this.drawGradientRect(j1, k1, j1 + 16, k1 + 16, -2130706433, -2130706433);
+					GlStateManager.colorMask(true, true, true, true);
+					GlStateManager.enableLighting();
+					GlStateManager.enableDepth();
+				}
+			}
+			
+			RenderHelper.disableStandardItemLighting();
+			this.drawGuiContainerForegroundLayer(mouseX, mouseY);
+			RenderHelper.enableGUIStandardItemLighting();
+			net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.client.event.GuiContainerEvent.DrawForeground(this, mouseX, mouseY));
+			InventoryPlayer inventoryplayer = this.mc.player.inventory;
+			ItemStack itemstack = this.draggedStack.isEmpty() ? inventoryplayer.getItemStack() : this.draggedStack;
+			
+			if(!itemstack.isEmpty())
+			{
+				int j2 = 8;
+				int k2 = this.draggedStack.isEmpty() ? 8 : 16;
+				String s = null;
+				
+				if(!this.draggedStack.isEmpty() && this.isRightMouseClick)
+				{
+					itemstack = itemstack.copy();
+					itemstack.setCount(MathHelper.ceil((float) itemstack.getCount() / 2.0F));
+				} else if(this.dragSplitting && this.dragSplittingSlots.size() > 1)
+				{
+					itemstack = itemstack.copy();
+					itemstack.setCount(this.dragSplittingRemnant);
+					
+					if(itemstack.isEmpty())
+					{
+						s = "" + TextFormatting.YELLOW + "0";
+					}
+				}
+				
+				this.drawItemStack$(itemstack, mouseX - i - 8, mouseY - j - k2, s);
+			}
+			
+			if(!this.returningStack.isEmpty())
+			{
+				float f = (float) (Minecraft.getSystemTime() - this.returningStackTime) / 100.0F;
+				
+				if(f >= 1.0F)
+				{
+					f = 1.0F;
+					this.returningStack = ItemStack.EMPTY;
+				}
+				
+				int l2 = this.returningStackDestSlot.xPos - this.touchUpX;
+				int i3 = this.returningStackDestSlot.yPos - this.touchUpY;
+				int l1 = this.touchUpX + (int) ((float) l2 * f);
+				int i2 = this.touchUpY + (int) ((float) i3 * f);
+				this.drawItemStack$(this.returningStack, l1, i2, (String) null);
+			}
+			
+			GlStateManager.popMatrix();
+			GlStateManager.enableLighting();
+			GlStateManager.enableDepth();
+			RenderHelper.enableStandardItemLighting();
+			
+		}
 		if(fixMojang)
 			renderHoveredToolTip(mouseX, mouseY);
+	}
+	
+	protected void drawItemStack$(ItemStack stack, int x, int y, String altText)
+	{
+		GlStateManager.translate(0.0F, 0.0F, 32.0F);
+		this.zLevel = 200.0F;
+		this.itemRender.zLevel = 200.0F;
+		net.minecraft.client.gui.FontRenderer font = stack.getItem().getFontRenderer(stack);
+		if(font == null)
+			font = fontRenderer;
+		this.itemRender.renderItemAndEffectIntoGUI(stack, x, y);
+		this.itemRender.renderItemOverlayIntoGUI(font, stack, x, y - (this.draggedStack.isEmpty() ? 0 : 8), altText);
+		this.zLevel = 0.0F;
+		this.itemRender.zLevel = 0.0F;
 	}
 	
 	@Override
@@ -117,22 +244,18 @@ public class GuiWTFMojang<T extends Container> extends GuiContainer
 		
 		if(!flag1)
 		{
-			if(flag)
-				drawRect(i, j, i + w, j + h, -2130706433);
 			GlStateManager.enableDepth();
 			GlStateManager.pushMatrix();
+			GlStateManager.translate(i, j, 0);
 			GlStateManager.scale(w / 16D, h / 16D, w / 16D);
-			this.itemRender.renderItemAndEffectIntoGUI(this.mc.player, itemstack, i, j);
-			this.itemRender.renderItemOverlayIntoGUI(this.fontRenderer, itemstack, i, j, s);
+			if(flag)
+				drawRect(0, 0, 16, 16, -2130706433);
+			this.itemRender.renderItemAndEffectIntoGUI(this.mc.player, itemstack, 0, 0);
+			this.itemRender.renderItemOverlayIntoGUI(this.fontRenderer, itemstack, 0, 0, s);
 			GlStateManager.popMatrix();
 		}
 		
 		this.itemRender.zLevel = 0.0F;
 		this.zLevel = 0.0F;
-	}
-	
-	@Override
-	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
-	{
 	}
 }
--- src/main/java/com/zeitheron/hammercore/internal/init/ManualHC.java
+++ src/main/java/com/zeitheron/hammercore/internal/init/ManualHC.java
@@ -4,8 +4,6 @@ import com.zeitheron.hammercore.bookAPI.fancy.ManualCategories;
 import com.zeitheron.hammercore.bookAPI.fancy.ManualEntry;
 import com.zeitheron.hammercore.bookAPI.fancy.ManualEntry.EnumEntryShape;
 import com.zeitheron.hammercore.bookAPI.fancy.pages.ManualTextPage;
-import com.zeitheron.hammercore.bookAPI.fancy.ManualPage;
-import com.zeitheron.hammercore.utils.web.URLLocation;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
@@ -20,8 +18,5 @@ public class ManualHC
 		
 		if(ItemsHC.WRENCH != null)
 			new ManualEntry("wrench", "hammercore", 0, 2, new ItemStack(ItemsHC.WRENCH)).setPages(new ManualTextPage("hc.manual_desc.wrench")).setShape(EnumEntryShape.ROUND).registerEntry();
-		
-		new ManualEntry("apengu", "hammercore", -1, -2, new URLLocation("https://i.imgur.com/64QhGiC.png")).setPages(new ManualTextPage("hc.manual_desc.apengu")).setShape(EnumEntryShape.HEX).setColor(0xFFFF0000).registerEntry();
-		new ManualEntry("zeitheron", "hammercore", 1, -2, new URLLocation("https://visage.surgeplay.com/head/256/63fa4c485b5640809727d5d59663e603")).setPages(new ManualTextPage("hc.manual_desc.zeitheron")).setShape(EnumEntryShape.HEX).setColor(0xFF00FFFF).registerEntry();
 	}
 }
