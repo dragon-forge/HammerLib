2a806c50d4eb1ebdaf704c2c27821a084090f920
Zeitheron <zeitheron.dr@gmail.com>
2020-01-08 19:09:20 +0200

    Fixed networking issue, mainly occuring in TerrariaCraft (missing EntityPlayerMP on server rarely)

--- src/main/java/com/zeitheron/hammercore/net/PacketContext.java
+++ src/main/java/com/zeitheron/hammercore/net/PacketContext.java
@@ -1,29 +1,30 @@
 package com.zeitheron.hammercore.net;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.Side;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 public class PacketContext
 {
 	@Nonnull
 	public final IContextSender sender;
-	
+
 	@Nonnull
 	public final PacketHolder holder;
-	
+
 	@Nullable
 	public final MinecraftServer server;
-	
+
 	@Nonnull
 	public final Side side;
-	
+
 	private IPacket reply;
-	
+
 	public PacketContext(IContextSender sender, PacketHolder holder, MinecraftServer server)
 	{
 		this.sender = sender;
@@ -31,18 +32,18 @@ public class PacketContext
 		this.server = server;
 		this.side = FMLCommonHandler.instance().getEffectiveSide();
 	}
-	
+
 	public PacketContext withReply(IPacket reply)
 	{
 		this.reply = reply;
 		return this;
 	}
-	
+
 	public IPacket getReply()
 	{
 		return reply;
 	}
-	
+
 	@Nullable
 	public EntityPlayerMP getSender()
 	{
@@ -52,44 +53,67 @@ public class PacketContext
 			return holder.getPlayer(server);
 		return null;
 	}
-	
-	public static interface IContextSender
+
+	public interface IContextSender
 	{
 		Side from();
-		
+
 		EntityPlayerMP asPlayer();
+
+		/**
+		 * This method retrieves either a packet sender (server), or a client player (client)
+		 */
+		default EntityPlayer asBoundPlayer()
+		{
+			return asPlayer();
+		}
 	}
-	
-	public static class ContextSenderServer implements IContextSender
+
+	public static class ContextSenderServer
+			implements IContextSender
 	{
+		final EntityPlayer player;
+
+		public ContextSenderServer(EntityPlayer player)
+		{
+			this.player = player;
+		}
+
 		@Override
 		public Side from()
 		{
 			return Side.SERVER;
 		}
-		
+
 		@Override
 		public EntityPlayerMP asPlayer()
 		{
 			return null;
 		}
+
+		@Override
+		public EntityPlayer asBoundPlayer()
+		{
+			return player;
+		}
 	}
-	
-	public static class ContextSenderPlayerMP implements IContextSender
+
+	public static class ContextSenderPlayerMP
+			implements IContextSender
 	{
 		public EntityPlayerMP player;
-		
+
 		public ContextSenderPlayerMP(EntityPlayerMP player)
 		{
 			this.player = player;
 		}
-		
+
 		@Override
 		public Side from()
 		{
 			return Side.CLIENT;
 		}
-		
+
 		@Override
 		public EntityPlayerMP asPlayer()
 		{
--- src/main/java/com/zeitheron/hammercore/net/PacketHolder.java
+++ src/main/java/com/zeitheron/hammercore/net/PacketHolder.java
@@ -1,14 +1,13 @@
 package com.zeitheron.hammercore.net;
 
-import java.util.UUID;
-
 import com.zeitheron.hammercore.HammerCore;
 import com.zeitheron.hammercore.net.PacketContext.ContextSenderPlayerMP;
 import com.zeitheron.hammercore.net.PacketContext.ContextSenderServer;
-
+import com.zeitheron.hammercore.utils.base.Cast;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientCustomPacketEvent;
@@ -16,13 +15,15 @@ import net.minecraftforge.fml.common.network.FMLNetworkEvent.ServerCustomPacketE
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.UUID;
+
 public class PacketHolder
 {
 	public final IPacket packet;
 	public final String playerName;
 	public final UUID playerUUID;
 	public final int playerId;
-	
+
 	public PacketHolder(NBTTagCompound nbt)
 	{
 		packet = HCNet.readPacket(nbt);
@@ -30,7 +31,7 @@ public class PacketHolder
 		playerUUID = nbt.getUniqueId("PU");
 		playerId = nbt.getInteger("PI");
 	}
-	
+
 	public PacketHolder(IPacket packet, EntityPlayer player)
 	{
 		this.packet = packet;
@@ -46,7 +47,7 @@ public class PacketHolder
 			this.playerId = -1;
 		}
 	}
-	
+
 	public PacketHolder(IPacket packet)
 	{
 		this.packet = packet;
@@ -54,7 +55,7 @@ public class PacketHolder
 		this.playerUUID = UUID.randomUUID();
 		this.playerId = -1;
 	}
-	
+
 	public NBTTagCompound writeToNBT(NBTTagCompound nbt)
 	{
 		nbt = HCNet.writePacket(packet, nbt);
@@ -63,35 +64,36 @@ public class PacketHolder
 		nbt.setInteger("PI", playerId);
 		return nbt;
 	}
-	
+
 	public boolean containsPlayer()
 	{
 		return playerId > 0;
 	}
-	
+
 	public boolean enforceMainThread()
 	{
 		return packet != null && packet.executeOnMainThread();
 	}
-	
+
 	@SideOnly(Side.CLIENT)
 	public PacketHolder execute(ClientCustomPacketEvent handler)
 	{
 		EntityPlayer player = HammerCore.renderProxy.getClientPlayer();
-		PacketContext ctx = new PacketContext(new ContextSenderServer(), this, null);
+		PacketContext ctx = new PacketContext(new ContextSenderServer(player), this, null);
 		packet.executeOnClient2(ctx);
 		return new PacketHolder(ctx.getReply(), player);
 	}
-	
+
 	public PacketHolder execute(ServerCustomPacketEvent handler)
 	{
 		MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
-		EntityPlayerMP mp = getPlayer(server);
+		NetHandlerPlayServer net = Cast.cast(handler.getHandler(), NetHandlerPlayServer.class);
+		EntityPlayerMP mp = net != null ? net.player : getPlayer(server);
 		PacketContext ctx = new PacketContext(new ContextSenderPlayerMP(mp), this, server);
 		packet.executeOnServer2(ctx);
 		return new PacketHolder(ctx.getReply());
 	}
-	
+
 	public EntityPlayerMP getPlayer(MinecraftServer server)
 	{
 		return server != null && containsPlayer() ? server.getPlayerList().getPlayerByUUID(playerUUID) : null;
