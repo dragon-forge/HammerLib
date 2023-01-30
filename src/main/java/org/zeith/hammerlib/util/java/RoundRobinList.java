b8bfc1ea7e225bce136ddc6ef8c1640ad5431964
Zeith <me@zeith.org>
2023-01-30 20:11:42 +0200

    QoL improvements for RoundRobinList

--- src/main/java/org/zeith/hammerlib/util/java/RoundRobinList.java
+++ src/main/java/org/zeith/hammerlib/util/java/RoundRobinList.java
@@ -1,17 +1,38 @@
 package org.zeith.hammerlib.util.java;
 
-import java.util.ArrayList;
+import javax.annotation.Nonnull;
+import java.util.*;
 
 public class RoundRobinList<E>
 		extends ArrayList<E>
 {
 	private int lastIndex;
 	
+	public RoundRobinList()
+	{
+	}
+	
+	public RoundRobinList(int initialCapacity)
+	{
+		super(initialCapacity);
+	}
+	
+	public RoundRobinList(@Nonnull Collection<? extends E> c)
+	{
+		super(c);
+	}
+	
 	public E next()
 	{
 		E e = get(lastIndex);
 		skip(1);
 		return e;
+	}
+	
+	public E prev()
+	{
+		backward(1);
+		return get(lastIndex);
 	}
 	
 	public void skip(int steps)
@@ -35,5 +56,15 @@ public class RoundRobinList<E>
 	public void setPos(int i)
 	{
 		lastIndex = Math.abs(i) % size();
+	}
+	
+	public static <T> RoundRobinList<T> of(T... arr)
+	{
+		return new RoundRobinList<>(List.of(arr));
+	}
+	
+	public static <T> RoundRobinList<T> copyOf(Collection<? extends T> coll)
+	{
+		return new RoundRobinList<>(coll);
 	}
 }
