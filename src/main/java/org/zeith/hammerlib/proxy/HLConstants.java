b8bd5d46fb4631b9b62872643da37bc1531d57f7
Zeith <me@zeith.org>
2023-06-09 15:09:54 +0300

    HammerLib tab should (hopefully) always be after vanilla tabs. Small refactors and improvements.

--- src/main/java/org/zeith/hammerlib/proxy/HLConstants.java
+++ src/main/java/org/zeith/hammerlib/proxy/HLConstants.java
@@ -1,24 +1,38 @@
 package org.zeith.hammerlib.proxy;
 
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.apache.logging.log4j.Logger;
 import org.zeith.hammerlib.HammerLib;
 import org.zeith.hammerlib.api.items.CreativeTab;
 import org.zeith.hammerlib.core.init.ItemsHL;
+import org.zeith.hammerlib.util.CommonMessages;
 
 public class HLConstants
 {
 	public static final Logger LOG = HammerLib.LOG;
 	public static final String MOD_ID = "hammerlib";
 	
-	public static final Component CRAFTING_MATERIAL = Component.translatable("info.hammerlib.material").withStyle(ChatFormatting.GRAY);
+	@Deprecated // This should have not been placed here in the first place.
+	public static final Component CRAFTING_MATERIAL = CommonMessages.CRAFTING_MATERIAL;
 	
 	@CreativeTab.RegisterTab
 	public static final CreativeTab HL_TAB = new CreativeTab(id("root"),
 			b -> b.icon(() -> ItemsHL.WRENCH.getDefaultInstance())
 					.title(Component.translatable("itemGroup." + MOD_ID))
+					.withTabsBefore(
+							CreativeModeTabs.BUILDING_BLOCKS,
+							CreativeModeTabs.COLORED_BLOCKS,
+							CreativeModeTabs.NATURAL_BLOCKS,
+							CreativeModeTabs.FUNCTIONAL_BLOCKS,
+							CreativeModeTabs.REDSTONE_BLOCKS,
+							CreativeModeTabs.TOOLS_AND_UTILITIES,
+							CreativeModeTabs.COMBAT,
+							CreativeModeTabs.FOOD_AND_DRINKS,
+							CreativeModeTabs.INGREDIENTS,
+							CreativeModeTabs.SPAWN_EGGS
+					)
 	);
 	
 	public static ResourceLocation id(String path)
--- src/main/java/org/zeith/hammerlib/util/ZeithLinkRepository.java
+++ src/main/java/org/zeith/hammerlib/util/ZeithLinkRepository.java
@@ -1,6 +1,7 @@
 package org.zeith.hammerlib.util;
 
-import net.minecraft.Util;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.loading.FMLLoader;
 import org.zeith.hammerlib.proxy.HLConstants;
 import org.zeith.hammerlib.util.java.net.HttpRequest;
 import org.zeith.hammerlib.util.mcf.ModHelper;
@@ -8,6 +9,8 @@ import org.zeith.hammerlib.util.shaded.json.JSONTokener;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 
 public class ZeithLinkRepository
 {
@@ -35,12 +38,17 @@ public class ZeithLinkRepository
 	
 	public static String getLink(PredefinedLink link)
 	{
-		return LINKS.get(link.key());
+		return Storage.LINKS.get(link.key());
 	}
 	
 	public static Optional<String> findLink(String key)
 	{
-		return Optional.ofNullable(LINKS.get(key));
+		return Optional.ofNullable(Storage.LINKS.get(key));
+	}
+	
+	public static Optional<String> findModLink(ResourceLocation path)
+	{
+		return findLink("mods/" + path.getNamespace() + "/" + path.getPath());
 	}
 	
 	private static final String DEFAULT_JSON = """
@@ -59,44 +67,136 @@ public class ZeithLinkRepository
 			}
 			""";
 	
-	private static final Map<String, String> LINKS = Util.make(new HashMap<>(), m ->
+	private interface Storage
 	{
-		final List<String> SOURCES = List.of(
-				"https://pastebin.com/raw/Wtfb9h01",
-				"https://mods.zeith.org/static/link_repository.json"
-		);
-		
-		CompletableFuture.runAsync(() ->
+		Map<String, String> LINKS = new HashMap<>()
 		{
-			for(var src : SOURCES)
 			{
-				try(var req = HttpRequest.get(src)
-						.acceptJson()
-						.userAgent("HammerLib " + ModHelper.getModVersion(HLConstants.MOD_ID) + "; Minecraft")
-						.connectTimeout(60000)
-						.readTimeout(60000)
-				)
+				final List<String> SOURCES = List.of(
+						"https://pastebin.com/raw/Wtfb9h01",
+						"https://gist.githubusercontent.com/Zeitheron/78b4f4745cbf14b3d5974ee7ef437249/raw/link_repository.json",
+						"https://mods.zeith.org/static/link_repository.json"
+				);
+				
+				Consumer<String> loader = json ->
 				{
-					if(req.ok())
-					{
-						for(var e : JSONVisitor.expand(new JSONTokener(req.body()).nextValue(), "/", false).entrySet())
-							m.put(e.getKey(), Objects.toString(e.getValue()));
-						return;
-					}
+					for(var e : JSONVisitor.expand(new JSONTokener(json).nextValue(), "/", false).entrySet())
+						super.put(e.getKey(), Objects.toString(e.getValue()));
+				};
+				
+				try
+				{
+					loader.accept(DEFAULT_JSON); // insta-load the default links baked into the mod, and then fetch more links.
 				} catch(Exception e)
 				{
 					// NO-OP
 				}
+				
+				CompletableFuture.runAsync(() ->
+				{
+					
+					for(var src : SOURCES)
+					{
+						try(var req = HttpRequest.get(src)
+								.acceptJson()
+								.userAgent("HammerLib " + ModHelper.getModVersion(HLConstants.MOD_ID) + "; Minecraft " + FMLLoader.versionInfo().mcVersion())
+								.connectTimeout(60000)
+								.readTimeout(60000)
+						)
+						{
+							if(req.ok())
+							{
+								loader.accept(req.body());
+								return;
+							}
+						} catch(Exception e)
+						{
+							// NO-OP
+						}
+					}
+				});
 			}
 			
-			try
+			@Override
+			public String put(String key, String value)
 			{
-				for(var e : JSONVisitor.expand(new JSONTokener(DEFAULT_JSON).nextValue(), "/", false).entrySet())
-					m.put(e.getKey(), Objects.toString(e.getValue()));
-			} catch(Exception e)
-			{
-				// NO-OP
+				return get(key);
 			}
-		});
-	});
+			
+			@Override
+			public void putAll(Map<? extends String, ? extends String> m)
+			{
+			}
+			
+			@Override
+			public String computeIfPresent(String key, BiFunction<? super String, ? super String, ? extends String> remappingFunction)
+			{
+				return get(key);
+			}
+			
+			@Override
+			public String compute(String key, BiFunction<? super String, ? super String, ? extends String> remappingFunction)
+			{
+				return get(key);
+			}
+			
+			@Override
+			public String merge(String key, String value, BiFunction<? super String, ? super String, ? extends String> remappingFunction)
+			{
+				return get(key);
+			}
+			
+			@Override
+			public void clear()
+			{
+			}
+			
+			@Override
+			public boolean remove(Object key, Object value)
+			{
+				return false;
+			}
+			
+			@Override
+			public String remove(Object key)
+			{
+				return get(key);
+			}
+			
+			@Override
+			public Set<Entry<String, String>> entrySet()
+			{
+				return Set.copyOf(super.entrySet());
+			}
+			
+			@Override
+			public Set<String> keySet()
+			{
+				return Set.copyOf(super.keySet());
+			}
+			
+			@Override
+			public Collection<String> values()
+			{
+				return List.copyOf(super.values());
+			}
+			
+			@Override
+			public boolean replace(String key, String oldValue, String newValue)
+			{
+				return false;
+			}
+			
+			@Override
+			public String replace(String key, String value)
+			{
+				return value;
+			}
+			
+			@Override
+			public void replaceAll(BiFunction<? super String, ? super String, ? extends String> function)
+			{
+			}
+		};
+	}
 }
