b3964d34fcfdcc6ecedc525556d71f2fcbe82d6d
Zeith <me@zeith.org>
2023-09-21 02:02:55 +0300

    Use forge VertexConsumerWrapper

--- src/main/java/org/zeith/hammerlib/client/render/TintingVertexConsumer.java
+++ src/main/java/org/zeith/hammerlib/client/render/TintingVertexConsumer.java
@@ -1,18 +1,13 @@
 package org.zeith.hammerlib.client.render;
 
-import com.mojang.blaze3d.vertex.*;
-import net.minecraft.client.renderer.block.model.BakedQuad;
-import org.joml.*;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraftforge.client.model.pipeline.VertexConsumerWrapper;
 import org.zeith.hammerlib.util.colors.ColorHelper;
 import org.zeith.hammerlib.util.java.functions.*;
 
-import java.nio.ByteBuffer;
-
 public class TintingVertexConsumer
-		implements VertexConsumer
+		extends VertexConsumerWrapper
 {
-	protected final VertexConsumer parent;
-	
 	protected float a, r, g, b;
 	
 	public static boolean tintingEnabled = true;
@@ -21,7 +16,7 @@ public class TintingVertexConsumer
 	
 	protected TintingVertexConsumer(VertexConsumer parent, int rgba)
 	{
-		this.parent = parent;
+		super(parent);
 		r = ColorHelper.getRed(rgba);
 		g = ColorHelper.getGreen(rgba);
 		b = ColorHelper.getBlue(rgba);
@@ -30,7 +25,7 @@ public class TintingVertexConsumer
 	
 	protected TintingVertexConsumer(VertexConsumer parent, float r, float g, float b, float a)
 	{
-		this.parent = parent;
+		super(parent);
 		this.r = r;
 		this.g = g;
 		this.b = b;
@@ -48,13 +43,6 @@ public class TintingVertexConsumer
 	}
 	
 	@Override
-	public VertexConsumer vertex(double pX, double pY, double pZ)
-	{
-		parent.vertex(pX, pY, pZ);
-		return this;
-	}
-	
-	@Override
 	public VertexConsumer color(int pRed, int pGreen, int pBlue, int pAlpha)
 	{
 		pRed *= r;
@@ -66,40 +54,6 @@ public class TintingVertexConsumer
 	}
 	
 	@Override
-	public VertexConsumer uv(float pU, float pV)
-	{
-		parent.uv(pU, pV);
-		return this;
-	}
-	
-	@Override
-	public VertexConsumer overlayCoords(int pU, int pV)
-	{
-		parent.overlayCoords(pU, pV);
-		return this;
-	}
-	
-	@Override
-	public VertexConsumer uv2(int pU, int pV)
-	{
-		parent.uv2(pU, pV);
-		return this;
-	}
-	
-	@Override
-	public VertexConsumer normal(float pX, float pY, float pZ)
-	{
-		parent.normal(pX, pY, pZ);
-		return this;
-	}
-	
-	@Override
-	public void endVertex()
-	{
-		parent.endVertex();
-	}
-	
-	@Override
 	public void defaultColor(int pDefaultR, int pDefaultG, int pDefaultB, int pDefaultA)
 	{
 		pDefaultR *= r;
@@ -107,120 +61,5 @@ public class TintingVertexConsumer
 		pDefaultB *= b;
 		pDefaultA *= a;
 		parent.defaultColor(pDefaultR, pDefaultG, pDefaultB, pDefaultA);
-	}
-	
-	@Override
-	public void unsetDefaultColor()
-	{
-		parent.unsetDefaultColor();
-	}
-	
-	@Override
-	public void vertex(float pX, float pY, float pZ, float pRed, float pGreen, float pBlue, float pAlpha, float pTexU, float pTexV, int pOverlayUV, int pLightmapUV, float pNormalX, float pNormalY, float pNormalZ)
-	{
-		parent.vertex(
-				pX, pY, pZ,
-				pRed * r, pGreen * g, pBlue * b, pAlpha * a,
-				pTexU, pTexV,
-				pOverlayUV, pLightmapUV,
-				pNormalX, pNormalY, pNormalZ
-		);
-	}
-	
-	@Override
-	public VertexConsumer color(float pRed, float pGreen, float pBlue, float pAlpha)
-	{
-		parent.color(pRed * r, pGreen * g, pBlue * b, pAlpha * a);
-		return this;
-	}
-	
-	@Override
-	public VertexConsumer color(int pColorARGB)
-	{
-		pColorARGB = ColorHelper.packARGB(
-				ColorHelper.getAlpha(pColorARGB) * a,
-				ColorHelper.getRed(pColorARGB) * r,
-				ColorHelper.getGreen(pColorARGB) * g,
-				ColorHelper.getBlue(pColorARGB) * b
-		);
-		parent.color(pColorARGB);
-		return this;
-	}
-	
-	@Override
-	public VertexConsumer uv2(int pLightmapUV)
-	{
-		parent.uv2(pLightmapUV);
-		return this;
-	}
-	
-	@Override
-	public VertexConsumer overlayCoords(int pOverlayUV)
-	{
-		parent.overlayCoords(pOverlayUV);
-		return this;
-	}
-	
-	@Override
-	public void putBulkData(PoseStack.Pose pPoseEntry, BakedQuad pQuad, float pRed, float pGreen, float pBlue, int pCombinedLight, int pCombinedOverlay)
-	{
-		parent.putBulkData(pPoseEntry, pQuad, pRed * r, pGreen * g, pBlue * b, pCombinedLight, pCombinedOverlay);
-	}
-	
-	@Override
-	public void putBulkData(PoseStack.Pose pPoseEntry, BakedQuad pQuad, float[] pColorMuls, float pRed, float pGreen, float pBlue, int[] pCombinedLights, int pCombinedOverlay, boolean pMulColor)
-	{
-		parent.putBulkData(pPoseEntry, pQuad, pColorMuls,
-				pRed * r, pGreen * g, pBlue * b, pCombinedLights, pCombinedOverlay, pMulColor
-		);
-	}
-	
-	@Override
-	public void putBulkData(PoseStack.Pose pPoseEntry, BakedQuad pQuad, float[] pColorMuls, float pRed, float pGreen, float pBlue, float alpha, int[] pCombinedLights, int pCombinedOverlay, boolean pMulColor)
-	{
-		parent.putBulkData(pPoseEntry, pQuad, pColorMuls,
-				pRed * r, pGreen * g, pBlue * b, alpha * a, pCombinedLights, pCombinedOverlay, pMulColor
-		);
-	}
-	
-	@Override
-	public VertexConsumer vertex(Matrix4f pMatrix, float pX, float pY, float pZ)
-	{
-		parent.vertex(pMatrix, pX, pY, pZ);
-		return this;
-	}
-	
-	@Override
-	public VertexConsumer normal(Matrix3f pMatrix, float pX, float pY, float pZ)
-	{
-		parent.normal(pMatrix, pX, pY, pZ);
-		return this;
-	}
-	
-	@Override
-	public VertexConsumer misc(VertexFormatElement element, int... rawData)
-	{
-		parent.misc(element, rawData);
-		return this;
-	}
-	
-	@Override
-	public void putBulkData(PoseStack.Pose pose, BakedQuad bakedQuad, float red, float green, float blue, float alpha, int packedLight, int packedOverlay, boolean readExistingColor)
-	{
-		parent.putBulkData(pose, bakedQuad,
-				red * r, green * g, blue * b, alpha * a, packedLight, packedOverlay, readExistingColor
-		);
-	}
-	
-	@Override
-	public int applyBakedLighting(int packedLight, ByteBuffer data)
-	{
-		return parent.applyBakedLighting(packedLight, data);
-	}
-	
-	@Override
-	public void applyBakedNormals(Vector3f generated, ByteBuffer data, Matrix3f normalTransform)
-	{
-		parent.applyBakedNormals(generated, data, normalTransform);
 	}
 }
