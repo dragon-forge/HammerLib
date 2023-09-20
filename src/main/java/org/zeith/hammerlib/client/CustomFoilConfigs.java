c379e8dce03403e6afa531e8d58854e5290a742a
Zeith <me@zeith.org>
2023-09-20 18:06:13 +0300

    InteModComms for custom foil.

--- src/main/java/org/zeith/hammerlib/client/CustomFoilConfigs.java
+++ src/main/java/org/zeith/hammerlib/client/CustomFoilConfigs.java
@@ -1,26 +1,95 @@
 package org.zeith.hammerlib.client;
 
 import net.minecraft.*;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.zeith.hammerlib.HammerLib;
 import org.zeith.hammerlib.api.items.IColoredFoilItem;
+import org.zeith.hammerlib.core.items.ItemWrench;
+import org.zeith.hammerlib.core.test.machine.BlockTestMachine;
 import org.zeith.hammerlib.proxy.HLConstants;
-import org.zeith.hammerlib.util.java.IOUtils;
+import org.zeith.hammerlib.util.java.*;
 import org.zeith.hammerlib.util.java.tuples.*;
 import org.zeith.hammerlib.util.shaded.json.*;
 
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
+import java.util.function.ToIntFunction;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CustomFoilConfigs
 {
 	private static final List<Tuple2<Item, IColoredFoilItem>> overrides = new ArrayList<>();
 	
-	@SuppressWarnings("deprecation")
+	@SubscribeEvent
+	public static void enqueueInterComms(InterModEnqueueEvent e)
+	{
+		// Constant color example
+		InterModComms.sendTo("hammerlib", "registerFoil",
+				() -> Map.<ItemLike, Integer>entry(ItemWrench.WRENCH, 0xFFFF0F | IColoredFoilItem.FULL_ALPHA)
+		);
+		
+		// Dynamic color example
+		InterModComms.sendTo("hammerlib", "registerFoil",
+				() -> Map.<ItemLike, ToIntFunction<ItemStack>>entry(BlockTestMachine.TEST_MACHINE, (ItemStack stack) -> 0x0F0FFF | IColoredFoilItem.FULL_ALPHA)
+		);
+	}
+	
+	@SubscribeEvent
+	public static void handleInterComms(InterModProcessEvent evt)
+	{
+		evt.getIMCStream("registerFoil"::equals).forEach(msg ->
+		{
+			ItemLike item = null;
+			
+			var obj = msg.messageSupplier().get();
+			if(obj instanceof Map.Entry<?, ?> e)
+			{
+				item = Cast.cast(e.getKey(), ItemLike.class);
+				obj = e.getValue();
+			}
+			
+			IColoredFoilItem foil = null;
+			String provider = "";
+			
+			if(obj instanceof Integer constant)
+			{
+				foil = IColoredFoilItem.constant(constant);
+				provider = "constant(0x" + Integer.toHexString(constant).toUpperCase(Locale.ROOT) + ")";
+			} else if(obj instanceof ToIntFunction<?> fun)
+			{
+				ToIntFunction<ItemStack> stack = (ToIntFunction<ItemStack>) fun;
+				foil = stack::applyAsInt;
+				provider = "dynamic(" + fun + ")";
+			}
+			
+			Item theItem;
+			//noinspection ConstantValue
+			if(item == null || (theItem = item.asItem()) == null)
+			{
+				HammerLib.LOG.warn("{} sent a {} IMC message, but the item can not be determined. Must be ItemLike that returns non-null item.", msg.senderModId(), msg.method());
+				return;
+			}
+			
+			if(foil == null)
+			{
+				HammerLib.LOG.warn("{} sent a {} IMC message, but the foil can not be determined. Must be either Integer, or ToIntFunction<ItemStack>.", msg.senderModId(), msg.method());
+				return;
+			}
+			
+			IColoredFoilItem.override(theItem, foil);
+			HammerLib.LOG.info("Performed {} from {}. Determined {} foil provider", msg.method(), msg.senderModId(), provider);
+		});
+	}
+	
 	public static void reload()
 			throws ReportedException
 	{
@@ -43,10 +112,10 @@ public class CustomFoilConfigs
 				
 				$.put("constant_colors",
 						new JSONObject()
-								.put(BuiltInRegistries.ITEM.getKey(Items.NETHER_STAR).toString(),
+								.put(Objects.toString(ForgeRegistries.ITEMS.getKey(Items.NETHER_STAR)),
 										0xFF185D | IColoredFoilItem.FULL_ALPHA
 								)
-								.put(BuiltInRegistries.ITEM.getKey(Items.ENCHANTED_GOLDEN_APPLE).toString(),
+								.put(Objects.toString(ForgeRegistries.ITEMS.getKey(Items.ENCHANTED_GOLDEN_APPLE)),
 										0xFDFF96 | IColoredFoilItem.FULL_ALPHA
 								)
 				);
@@ -67,7 +136,7 @@ public class CustomFoilConfigs
 					continue;
 				}
 				
-				var item = BuiltInRegistries.ITEM.get(loc);
+				var item = ForgeRegistries.ITEMS.getValue(loc);
 				if(item == null || item == Items.AIR)
 				{
 					HammerLib.LOG.info("foil_colors.json/constant_colors: Unable to find item " + loc + ". Skipping.");
