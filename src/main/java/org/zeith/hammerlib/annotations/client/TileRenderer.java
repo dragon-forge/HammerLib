ef08f746b0a1070deab8b0be77ae6feb1e55ceb8
Zeith <me@zeith.org>
2023-09-25 16:19:19 +0300

    Common wrapper for Particles and TileRenderer. This is for easier object gathering.

--- src/main/java/org/zeith/hammerlib/annotations/client/TileRenderer.java
+++ src/main/java/org/zeith/hammerlib/annotations/client/TileRenderer.java
@@ -1,6 +1,5 @@
 package org.zeith.hammerlib.annotations.client;
 
-import net.minecraft.Util;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import org.jetbrains.annotations.ApiStatus;
@@ -10,7 +9,6 @@ import org.zeith.hammerlib.client.render.tile.IBESR;
 import org.zeith.hammerlib.util.mcf.ScanDataHelper;
 
 import java.lang.annotation.*;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -23,33 +21,22 @@ public @interface TileRenderer
 	Class<?> value();
 	
 	@ApiStatus.Internal
-	record RendererTarget(Type owner, String field)
+	record Info(Runnable applicant)
 	{
-		public static final Map<TileRenderer.RendererTarget, TileRenderer.RendererInfo> TILE_RENDERS = DistExecutor.unsafeCallWhenOn(Dist.CLIENT, () -> () -> Util.make(new HashMap<>(), map ->
-		{
-			ScanDataHelper.lookupAnnotatedObjects(TileRenderer.class).forEach(data ->
-			{
-				if(data.getTargetType() == ElementType.FIELD)
-					data.getOwnerMod()
-							.ifPresent(mc ->
-									map.put(new TileRenderer.RendererTarget(data.clazz(), data.getMemberName()), new TileRenderer.RendererInfo(
-											() -> mc.getEventBus().addListener(
-													HammerLib.PROXY.addTESR(data.clazz(), data.getMemberName(), data.getProperty("value").map(Type.class::cast).orElse(null))
-											)
-									))
-							);
-			});
-		}));
+		public static final Map<ScanDataHelper.ScanTarget, Info> TILE_RENDERS = DistExecutor.unsafeCallWhenOn(Dist.CLIENT, () -> () -> ScanDataHelper.gatherScans(TileRenderer.class, ElementType.FIELD, (target, mc, data) ->
+				new Info(
+						() -> mc.getEventBus().addListener(
+								HammerLib.PROXY.addTESR(data.clazz(), data.getMemberName(), data.getProperty("value")
+										.map(Type.class::cast).orElse(null))
+						)
+				)
+		));
 		
-		public static RendererInfo get(Class<?> cls, String field)
+		public static Info get(Class<?> cls, String field)
 		{
-			return TILE_RENDERS.get(new RendererTarget(Type.getType(cls), field));
+			return TILE_RENDERS.get(ScanDataHelper.ScanTarget.of(cls, field));
 		}
-	}
-	
-	@ApiStatus.Internal
-	record RendererInfo(Runnable applicant)
-	{
+		
 		public void apply()
 		{
 			applicant.run();
