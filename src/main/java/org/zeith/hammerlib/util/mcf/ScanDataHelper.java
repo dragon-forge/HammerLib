d3acd98e950fd489f40a62ca24683cbcf30b1f3c
Zeith <me@zeith.org>
2023-09-25 16:19:19 +0300

    Common wrapper for Particles and TileRenderer. This is for easier object gathering.

--- src/main/java/org/zeith/hammerlib/util/mcf/ScanDataHelper.java
+++ src/main/java/org/zeith/hammerlib/util/mcf/ScanDataHelper.java
@@ -2,18 +2,16 @@ package org.zeith.hammerlib.util.mcf;
 
 import com.google.common.base.Predicates;
 import com.google.common.collect.ImmutableMap;
+import net.minecraft.Util;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.javafmlmod.FMLModContainer;
-import net.minecraftforge.forgespi.language.IModInfo;
-import net.minecraftforge.forgespi.language.ModFileScanData;
+import net.minecraftforge.forgespi.language.*;
 import net.minecraftforge.forgespi.language.ModFileScanData.AnnotationData;
 import org.objectweb.asm.Type;
-import org.zeith.hammerlib.util.java.Cast;
-import org.zeith.hammerlib.util.java.Fetcher;
-import org.zeith.hammerlib.util.java.ReflectionUtil;
+import org.zeith.hammerlib.util.java.*;
+import org.zeith.hammerlib.util.java.functions.Function3;
 
-import java.lang.annotation.Annotation;
-import java.lang.annotation.ElementType;
+import java.lang.annotation.*;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -24,25 +22,27 @@ public class ScanDataHelper
 	{
 		return lookupAnnotatedTypes(annotation, baseType, Predicates.alwaysTrue());
 	}
-
+	
 	/**
-	 * matcher can be an instance of {@link ScanProperties}. Use
-	 * {@link #properties(Object...)} to easily generate a matcher.
+	 * matcher can be an instance of {@link ScanProperties}.
+	 * Use {@link #properties(Object...)} to easily generate a matcher.
 	 */
 	public static <T> List<Class<? extends T>> lookupAnnotatedTypes(Class<? extends Annotation> annotation, Class<T> baseType, Predicate<ModAwareAnnotationData> matcher)
 	{
 		List<Class<? extends T>> classes = new ArrayList<>();
 		lookupAnnotatedObjects(annotation,
-				ad -> ad.parent.targetType() == ElementType.TYPE && baseType.isAssignableFrom(ReflectionUtil.fetchClass(ad.parent.clazz())) && matcher.test(ad)) //
+				ad -> ad.parent.targetType() == ElementType.TYPE &&
+						baseType.isAssignableFrom(ReflectionUtil.fetchClass(ad.parent.clazz())) && matcher.test(ad)
+		) //
 				.forEach(ad -> classes.add(ReflectionUtil.fetchClass(ad.parent.clazz())));
 		return classes;
 	}
-
+	
 	public static Collection<ModAwareAnnotationData> lookupAnnotatedObjects(Class<? extends Annotation> annotation)
 	{
 		return lookupAnnotatedObjects(annotation, Predicates.alwaysTrue());
 	}
-
+	
 	public static Collection<ModAwareAnnotationData> lookupAnnotatedObjects(Class<? extends Annotation> annotation, Predicate<ModAwareAnnotationData> matcher)
 	{
 		List<ModAwareAnnotationData> data = new ArrayList<>();
@@ -53,12 +53,12 @@ public class ScanDataHelper
 				.forEach(data::add);
 		return data;
 	}
-
+	
 	public static Collection<ModAwareAnnotationData> lookupAnnotatedObjects(ModFileScanData data, Class<? extends Annotation> annotation)
 	{
 		return lookupAnnotatedObjects(data, annotation, Predicates.alwaysTrue());
 	}
-
+	
 	public static Collection<ModAwareAnnotationData> lookupAnnotatedObjects(ModFileScanData data, Class<? extends Annotation> annotation, Predicate<ModAwareAnnotationData> matcher)
 	{
 		List<ModAwareAnnotationData> lst = new ArrayList<>();
@@ -69,7 +69,7 @@ public class ScanDataHelper
 				.forEach(lst::add);
 		return lst;
 	}
-
+	
 	/**
 	 * Create a predicate for matching {@link AnnotationData} with passed
 	 * key-values! <br>
@@ -79,12 +79,29 @@ public class ScanDataHelper
 	{
 		return new ScanProperties(keyValuePairs);
 	}
-
+	
+	public static <T, A extends Annotation> Map<ScanTarget, T> gatherScans(Class<A> annotation, ElementType target, Function3<ScanTarget, FMLModContainer, ModAwareAnnotationData, T> result)
+	{
+		return Util.make(ImmutableMap.<ScanTarget, T>builder(), map ->
+		{
+			for(var data : lookupAnnotatedObjects(annotation))
+			{
+				if(data.getTargetType() == target)
+				{
+					var mod = data.getOwnerMod().orElse(null);
+					if(mod == null) continue;
+					var tg = new ScanTarget(data.clazz(), data.getMemberName());
+					map.put(tg, result.apply(tg, mod, data));
+				}
+			}
+		}).build();
+	}
+	
 	public static class ScanProperties
 			implements Predicate<AnnotationData>
 	{
 		private final Map<String, Object> entries;
-
+		
 		public ScanProperties(Object... keyValuePairs)
 		{
 			if(keyValuePairs.length % 2 != 0)
@@ -95,12 +112,13 @@ public class ScanDataHelper
 				String key = Cast.cast(keyValuePairs[i], String.class);
 				Object value = keyValuePairs[i + 1];
 				if(key == null)
-					throw new NullPointerException("Key argument @" + i + " is " + (keyValuePairs[i] == null ? "null" : "not a string") + "!");
+					throw new NullPointerException(
+							"Key argument @" + i + " is " + (keyValuePairs[i] == null ? "null" : "not a string") + "!");
 				builder.put(key, value);
 			}
 			this.entries = builder.build();
 		}
-
+		
 		public boolean matches(Map<String, Object> data)
 		{
 			if(entries.size() > data.size())
@@ -112,27 +130,27 @@ public class ScanDataHelper
 					return false;
 			return true;
 		}
-
+		
 		@Override
 		public boolean test(AnnotationData t)
 		{
 			return matches(t.annotationData());
 		}
 	}
-
+	
 	public static class ModAwareAnnotationData
 	{
 		private final ModFileScanData modFile;
 		private Fetcher<Optional<FMLModContainer>> ownerMod;
-
+		
 		public final AnnotationData parent;
-
+		
 		public ModAwareAnnotationData(AnnotationData parent, ModFileScanData modFile)
 		{
 			this.parent = parent;
 			this.modFile = modFile;
 		}
-
+		
 		public Optional<FMLModContainer> getOwnerMod()
 		{
 			if(ownerMod == null)
@@ -146,39 +164,47 @@ public class ScanDataHelper
 						.findFirst()
 						.map(FMLModContainer.class::cast));
 			}
-
+			
 			return ownerMod.get();
 		}
-
+		
 		public ModFileScanData getModFile()
 		{
 			return modFile;
 		}
-
+		
 		public Class<?> getOwnerClass()
 		{
 			return ReflectionUtil.fetchClass(parent.clazz());
 		}
-
+		
 		public Type clazz()
 		{
 			return parent.clazz();
 		}
-
+		
 		public Optional<Object> getProperty(String key)
 		{
 			Map<String, Object> map = parent.annotationData();
 			return map.containsKey(key) ? Optional.ofNullable(map.get(key)) : Optional.empty();
 		}
-
+		
 		public ElementType getTargetType()
 		{
 			return parent.targetType();
 		}
-
+		
 		public String getMemberName()
 		{
 			return parent.memberName();
+		}
+	}
+	
+	public record ScanTarget(Type owner, String member)
+	{
+		public static ScanTarget of(Class<?> cls, String member)
+		{
+			return new ScanTarget(Type.getType(cls), member);
 		}
 	}
 }
