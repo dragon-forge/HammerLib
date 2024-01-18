67626e5eb5dc20d74236781ea4ab5cf676bd1069
Zeith <me@zeith.org>
2024-01-18 16:41:38 +0200

    Proxy creation API [1.20.1]

---
+++ src/main/java/org/zeith/hammerlib/api/proxy/IProxy.java
@@ -0,0 +1,31 @@
+package org.zeith.hammerlib.api.proxy;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import org.zeith.hammerlib.util.java.Cast;
+
+import java.util.function.Supplier;
+
+public interface IProxy
+{
+	default Player getClientPlayer()
+	{
+		return null;
+	}
+	
+	default Level getClientLevel()
+	{
+		return null;
+	}
+	
+	static <T extends IProxy> T create(Supplier<Supplier<T>> clientTarget, Supplier<Supplier<T>> serverTarget)
+	{
+		return switch(FMLEnvironment.dist)
+		{
+			case CLIENT -> Cast.cast(((Supplier) clientTarget.get()).get());
+			case DEDICATED_SERVER -> Cast.cast(((Supplier) serverTarget.get()).get());
+			default -> throw new IllegalArgumentException("UNSIDED?");
+		};
+	}
+}
