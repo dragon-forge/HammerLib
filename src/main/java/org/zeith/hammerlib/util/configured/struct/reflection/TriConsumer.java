3b4d27efcf1e9343796f9aaf58c9249e38dd25a0
Zeith <me@zeith.org>
2024-01-18 16:46:59 +0200

    Deprecate TriConsumer [1.19.2]

--- src/main/java/org/zeith/hammerlib/util/configured/struct/reflection/TriConsumer.java
+++ src/main/java/org/zeith/hammerlib/util/configured/struct/reflection/TriConsumer.java
@@ -1,5 +1,9 @@
 package org.zeith.hammerlib.util.configured.struct.reflection;
 
+/**
+ * @deprecated Use {@link org.zeith.hammerlib.util.java.consumers.Consumer3} instead.
+ */
+@Deprecated(forRemoval = true) // TODO: remove
 public interface TriConsumer<A, B, C>
 {
 	void accept(A a, B b, C c);
