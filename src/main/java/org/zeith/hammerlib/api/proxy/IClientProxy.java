223b4c3c7d5bd166c065bbfe70be669970d44e5e
Zeith <me@zeith.org>
2024-04-30 16:15:42 +0300

    [1.20.2] Proxy creation API

---
+++ src/main/java/org/zeith/hammerlib/api/proxy/IClientProxy.java
@@ -0,0 +1,22 @@
+package org.zeith.hammerlib.api.proxy;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import org.zeith.hammerlib.HammerLib;
+
+public interface IClientProxy
+		extends IProxy
+{
+	@Override
+	default Player getClientPlayer()
+	{
+		return HammerLib.PROXY.getClientPlayer();
+	}
+	
+	@Override
+	default Level getClientLevel()
+	{
+		var p = getClientPlayer();
+		return p != null ? p.level() : null;
+	}
+}
