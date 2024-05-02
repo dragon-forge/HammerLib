08152f5e7aada71907ccffcc0b56da70d0d6ff61
Zeith <me@zeith.org>
2024-05-02 06:18:24 +0300

    [1.20.5] Add ILevelActionType.simple

--- src/main/java/org/zeith/hammerlib/abstractions/actions/ILevelActionType.java
+++ src/main/java/org/zeith/hammerlib/abstractions/actions/ILevelActionType.java
@@ -4,6 +4,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 import org.zeith.hammerlib.core.RegistriesHL;
+import org.zeith.hammerlib.util.java.functions.Function3;
 
 /**
  * Represents a type of {@link LevelAction}, used to create new instances from NBT.
@@ -18,5 +19,17 @@ public interface ILevelActionType
 	default ResourceLocation getRegistryKey()
 	{
 		return RegistriesHL.levelActions().getKey(this);
+	}
+	
+	static ILevelActionType simple(Function3<ILevelActionType, Level, CompoundTag, LevelAction> factory)
+	{
+		return new ILevelActionType()
+		{
+			@Override
+			public LevelAction read(Level level, CompoundTag tag)
+			{
+				return factory.apply(this, level, tag);
+			}
+		};
 	}
 }
