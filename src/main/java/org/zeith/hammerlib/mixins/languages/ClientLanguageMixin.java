2685a5b7aec8799a5c289cbbb0b6b115a0342092
Zeith <me@zeith.org>
2024-07-03 19:26:27 +0300

    Fix #61 (crash with new Neo)

--- src/main/java/org/zeith/hammerlib/mixins/languages/ClientLanguageMixin.java
+++ src/main/java/org/zeith/hammerlib/mixins/languages/ClientLanguageMixin.java
@@ -3,10 +3,12 @@ package org.zeith.hammerlib.mixins.languages;
 import net.minecraft.client.resources.language.ClientLanguage;
 import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.zeith.hammerlib.api.LanguageHelper;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 @Mixin(ClientLanguage.class)
 public class ClientLanguageMixin
@@ -15,8 +17,10 @@ public class ClientLanguageMixin
 			method = "loadFrom",
 			at = @At(
 					value = "INVOKE",
-					target = "Lcom/google/common/collect/ImmutableMap;copyOf(Ljava/util/Map;)Lcom/google/common/collect/ImmutableMap;"
-			)
+					target = "Lcom/google/common/collect/ImmutableMap;copyOf(Ljava/util/Map;)Lcom/google/common/collect/ImmutableMap;",
+					ordinal = 0
+			),
+			index = 3
 	)
 	private static Map<String, String> map(Map<String, String> v, ResourceManager resources, List<String> languages)
 	{
