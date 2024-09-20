c9e8b9c07dc7b9a9a93491d15b65772d021e24b5
Zeith <me@zeith.org>
2024-09-20 20:14:20 +0300

    [1.20.1] Added ability to change click sound in GuiButtonObject;

--- src/main/java/org/zeith/hammerlib/client/flowgui/objects/GuiButtonObject.java
+++ src/main/java/org/zeith/hammerlib/client/flowgui/objects/GuiButtonObject.java
@@ -9,7 +9,10 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
 import net.minecraftforge.api.distmarker.Dist;
@@ -28,6 +31,7 @@ public class GuiButtonObject
 	public boolean enabled;
 	public Component message;
 	public OnPress callback;
+	public Holder<SoundEvent> pressSound;
 	
 	@Builder
 	public GuiButtonObject(@NotNull String name,
@@ -35,7 +39,8 @@ public class GuiButtonObject
 						   int packedFGColor,
 						   boolean enabled,
 						   @NotNull Component message,
-						   @NotNull OnPress callback
+						   @NotNull OnPress callback,
+						   Holder<SoundEvent> pressSound
 	)
 	{
 		super(name);
@@ -44,6 +49,7 @@ public class GuiButtonObject
 		this.enabled = enabled;
 		this.message = message;
 		this.callback = callback;
+		this.pressSound = pressSound;
 	}
 	
 	public GuiButtonObject setAlpha(float alpha)
@@ -78,7 +84,8 @@ public class GuiButtonObject
 				.packedFGColor(UNSET_FG_COLOR)
 				.enabled(true)
 				.message(Component.empty())
-				.callback(OnPress.NONE);
+				.callback(OnPress.NONE)
+				.pressSound(SoundEvents.UI_BUTTON_CLICK);
 	}
 	
 	@Override
@@ -117,7 +124,8 @@ public class GuiButtonObject
 	
 	public void playDownSound(SoundManager pHandler)
 	{
-		pHandler.play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.0F));
+		if(pressSound != null)
+			pHandler.play(SimpleSoundInstance.forUI(pressSound, 1.0F));
 	}
 	
 	private int getTextureY(boolean hovered)
