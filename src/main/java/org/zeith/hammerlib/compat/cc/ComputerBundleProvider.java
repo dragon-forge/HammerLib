98c1ce03cf79de2b995baa67dcf5eb75462e6b0a
Zeith <me@zeith.org>
2024-12-17 07:39:58 +0200

    [1.21.4] Bring back CC Tweaked compat!

--- src/main/java/org/zeith/hammerlib/compat/cc/ComputerBundleProvider.java
+++ src/main/java/org/zeith/hammerlib/compat/cc/ComputerBundleProvider.java
@@ -1,59 +1,50 @@
-//package org.zeith.hammerlib.compat.cc;
-//
-//import dan200.computercraft.shared.common.IBundledRedstoneBlock;
-//import dan200.computercraft.shared.computer.blocks.AbstractComputerBlockEntity;
-//import net.minecraft.core.Direction;
-//import org.jetbrains.annotations.Range;
-//import org.zeith.api.blocks.redstone.*;
-//
-//public class ComputerBundleProvider
-//		implements IRedstoneBundle
-//{
-//	public final AbstractComputerBlockEntity computer;
-//	public final Direction side;
-//
-//	public ComputerBundleProvider(AbstractComputerBlockEntity computer, Direction side)
-//	{
-//		this.computer = computer;
-//		this.side = side;
-//	}
-//
-//	@Override
-//	public boolean isConnected()
-//	{
-//		return computer.getBlockState().getBlock() instanceof IBundledRedstoneBlock blk;
-//	}
-//
-//	@Override
-//	public @Range(from = 0, to = 65535) int getSerializedBundleSignal()
-//	{
-//		if(computer.getBlockState().getBlock() instanceof IBundledRedstoneBlock blk)
-//			return blk.getBundledRedstoneOutput(computer.getLevel(), computer.getBlockPos(), side);
-//
-//		return 0;
-//	}
-//
-//	@Override
-//	public boolean hasSignal(MCColor color)
-//	{
-//		if(computer.getBlockState().getBlock() instanceof IBundledRedstoneBlock blk)
-//		{
-//			var redstone = blk.getBundledRedstoneOutput(computer.getLevel(), computer.getBlockPos(), side);
-//			return IRedstoneBundleAccessor.hasSerialized(redstone, color);
-//		}
-//
-//		return false;
-//	}
-//
-//	@Override
-//	public boolean setSignal(MCColor color, boolean signal)
-//	{
-//		return false;
-//	}
-//
-//	@Override
-//	public boolean setSerializedBundleSignal(@Range(from = 0, to = 65535) int serialized)
-//	{
-//		return false;
-//	}
-//}
+package org.zeith.hammerlib.compat.cc;
+
+import dan200.computercraft.shared.common.IBundledRedstoneBlock;
+import dan200.computercraft.shared.computer.blocks.AbstractComputerBlockEntity;
+import net.minecraft.core.Direction;
+import org.jetbrains.annotations.Range;
+import org.zeith.api.blocks.redstone.*;
+
+public record ComputerBundleProvider(AbstractComputerBlockEntity computer, Direction side)
+		implements IRedstoneBundle
+{
+	@Override
+	public boolean isConnected()
+	{
+		return computer.getBlockState().getBlock() instanceof IBundledRedstoneBlock blk;
+	}
+	
+	@Override
+	public @Range(from = 0, to = 65535) int getSerializedBundleSignal()
+	{
+		if(computer.getBlockState().getBlock() instanceof IBundledRedstoneBlock blk)
+			return blk.getBundledRedstoneOutput(computer.getLevel(), computer.getBlockPos(), side);
+		
+		return 0;
+	}
+	
+	@Override
+	public boolean hasSignal(MCColor color)
+	{
+		if(computer.getBlockState().getBlock() instanceof IBundledRedstoneBlock blk)
+		{
+			var redstone = blk.getBundledRedstoneOutput(computer.getLevel(), computer.getBlockPos(), side);
+			return IRedstoneBundleAccessor.hasSerialized(redstone, color);
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public boolean setSignal(MCColor color, boolean signal)
+	{
+		return false;
+	}
+	
+	@Override
+	public boolean setSerializedBundleSignal(@Range(from = 0, to = 65535) int serialized)
+	{
+		return false;
+	}
+}
--- src/main/java/org/zeith/hammerlib/compat/cc/HammerLibCCRedstoneProvider.java
+++ src/main/java/org/zeith/hammerlib/compat/cc/HammerLibCCRedstoneProvider.java
@@ -1,20 +1,19 @@
-//package org.zeith.hammerlib.compat.cc;
-//
-//import dan200.computercraft.api.redstone.BundledRedstoneProvider;
-//import net.minecraft.core.*;
-//import net.minecraft.world.level.Level;
-//import org.zeith.api.blocks.redstone.*;
-//
-//
-//public class HammerLibCCRedstoneProvider
-//		implements BundledRedstoneProvider
-//{
-//	@Override
-//	public int getBundledRedstoneOutput(Level level, BlockPos pos, Direction side)
-//	{
-//		var cap = level.getCapability(RedstoneBundleCapability.BLOCK, pos, side);
-//		if(cap != null)
-//			return IRedstoneBundleAccessor.getSerializedBundleSignal(cap);
-//		return -1;
-//	}
-//}
+package org.zeith.hammerlib.compat.cc;
+
+import dan200.computercraft.api.redstone.BundledRedstoneProvider;
+import net.minecraft.core.*;
+import net.minecraft.world.level.Level;
+import org.zeith.api.blocks.redstone.*;
+
+public class HammerLibCCRedstoneProvider
+		implements BundledRedstoneProvider
+{
+	@Override
+	public int getBundledRedstoneOutput(Level level, BlockPos pos, Direction side)
+	{
+		var cap = level.getCapability(RedstoneBundleCapability.BLOCK, pos, side);
+		if(cap != null)
+			return IRedstoneBundleAccessor.getSerializedBundleSignal(cap);
+		return -1;
+	}
+}
