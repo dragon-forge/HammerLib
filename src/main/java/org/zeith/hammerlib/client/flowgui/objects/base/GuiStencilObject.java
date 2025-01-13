61c3b94bab5bec1aa9c240e4adbb0cbb9491355b
Zeith <me@zeith.org>
2025-01-13 03:08:09 +0200

    GuiStencilObject non-abstract

--- src/main/java/org/zeith/hammerlib/client/flowgui/objects/base/GuiStencilObject.java
+++ src/main/java/org/zeith/hammerlib/client/flowgui/objects/base/GuiStencilObject.java
@@ -8,7 +8,7 @@ import org.zeith.hammerlib.util.math.Point;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class GuiStencilObject
+public class GuiStencilObject
 		extends GuiObject
 {
 	protected final List<GuiObject> stencilChildren = new ArrayList<>();
