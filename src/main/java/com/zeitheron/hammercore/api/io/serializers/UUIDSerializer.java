355f5e9be23663d7adf4342bf47f8970bb235dfc
Zeith <me@zeith.org>
2026-06-07 19:06:16 +0300

    12.2.62 - Fix UUID NBT serialization to use int[4] (as single property) instead of old 1.12.2 two property approach (Most/Least suffixes).

--- src/main/java/com/zeitheron/hammercore/api/io/serializers/UUIDSerializer.java
+++ src/main/java/com/zeitheron/hammercore/api/io/serializers/UUIDSerializer.java
@@ -6,6 +6,7 @@ import net.minecraftforge.common.util.Constants;
 
 import javax.annotation.Nonnull;
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @NBTSerializer(UUID.class)
 public class UUIDSerializer
@@ -15,12 +16,50 @@ public class UUIDSerializer
 	public void serialize(NBTTagCompound nbt, String key, @Nonnull UUID value)
 	{
 		if(value != null)
-			nbt.setUniqueId(key, value);
+			nbt.setIntArray(key, encode(value));
 	}
 	
 	@Override
 	public UUID deserialize(NBTTagCompound nbt, String key)
 	{
-		return nbt.hasKey(key, Constants.NBT.TAG_INT_ARRAY) ? nbt.getUniqueId(key) : null;
+		int[] ints;
+		return nbt.hasKey(key, Constants.NBT.TAG_INT_ARRAY) && (ints = nbt.getIntArray(key)).length == 4 ? decode(ints) : null;
+	}
+	
+	public static int[] encode(UUID uuid)
+	{
+		long msb = uuid.getMostSignificantBits();
+		long lsb = uuid.getLeastSignificantBits();
+		
+		return new int[] {
+				(int) (msb >>> 32),
+				(int) msb,
+				(int) (lsb >>> 32),
+				(int) lsb
+		};
+	}
+	
+	public static UUID decode(int[] data)
+	{
+		if(data == null || data.length < 4) return null;
+		long msb = ((long) data[0] << 32) | (data[1] & 0xFFFFFFFFL);
+		long lsb = ((long) data[2] << 32) | (data[3] & 0xFFFFFFFFL);
+		return new UUID(msb, lsb);
+	}
+	
+	public static UUID decodeOrDefault(int[] data, UUID defaultValue)
+	{
+		if(data == null || data.length < 4) return defaultValue;
+		long msb = ((long) data[0] << 32) | (data[1] & 0xFFFFFFFFL);
+		long lsb = ((long) data[2] << 32) | (data[3] & 0xFFFFFFFFL);
+		return new UUID(msb, lsb);
+	}
+	
+	public static UUID decodeOrGet(int[] data, Supplier<UUID> defaultValue)
+	{
+		if(data == null || data.length < 4) return defaultValue.get();
+		long msb = ((long) data[0] << 32) | (data[1] & 0xFFFFFFFFL);
+		long lsb = ((long) data[2] << 32) | (data[3] & 0xFFFFFFFFL);
+		return new UUID(msb, lsb);
 	}
 }
